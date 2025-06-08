5013dc146c083b4d430b5dae29eae3777f8708be
sturdy3299 <c6k6z0zf@anonaddy.me>
2025-06-08 19:38:32 +0200

    Mount game drive on desktop

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -15,5 +15,11 @@
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.useOSProber = true;
 
+  fileSystems."/mnt/games" =
+    { device = "/dev/disk/by-uuid/d13ee898-8082-4557-b8f3-90dcc68c94de";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+
   networking.hostName = "desktop";
 }
