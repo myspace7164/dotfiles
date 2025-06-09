19115b7fd965dcf0afe374eceaab7b6c6f5bfa7c
sturdy3299 <c6k6z0zf@anonaddy.me>
2025-06-09 10:50:53 +0200

    Update arkenfox script

--- scripts/arkenfox.sh
+++ scripts/arkenfox.sh
@@ -1,23 +1,55 @@
 #!/usr/bin/env bash
 
 basedir=$(dirname "$0")
+echo $basedir
 
-echo "basedir=$basedir"
+# Set Firefox profile directory
+moz_dir="$HOME/.mozilla/firefox"
+user_overrides_source="$(pwd)/$basedir/../config/user-overrides.js"
+profile_suffix="arkenfox"
 
-# TODO Maybe verify download?
-echo "Downloading updater.sh to $basedir"
-curl -o $basedir/updater.sh https://raw.githubusercontent.com/arkenfox/user.js/master/updater.sh
+# Look for existing *.arkenfox profile
+profile_dir=$(find "$moz_dir" -maxdepth 1 -type d -name "*.$profile_suffix" | head -n 1)
 
-echo "Downloading prefsCleaner.sh to $basedir"
-curl -o $basedir/prefsCleaner.sh https://raw.githubusercontent.com/arkenfox/user.js/master/prefsCleaner.sh
+# If no profile found, start Firefox to generate profiles
+if [ -z "$profile_dir" ]; then
+    if [[ -z "$DISPLAY" && -z "$WAYLAND_DISPLAY" ]]; then
+        echo "Error: No graphical session detected. Cannot start Firefox."
+        exit 1
+    fi
 
-bash $basedir/updater.sh -s -u
-bash $basedir/prefsCleaner.sh -s
+    echo "No .arkenfox profile found. Starting Firefox to create one..."
+    echo "Make sure you name the profile: $profile_suffix"
+    firefox --ProfileManager
 
-echo "Deleting $basedir/updater.sh"
-rm -vf $basedir/updater.sh
+    # Try finding the profile again
+    profile_dir=$(find "$moz_dir" -maxdepth 1 -type d -name "*.$profile_suffix" | head -n 1)
 
-echo "Deleting $basedir/prefsCleaner.sh"
-rm -vf $basedir/prefsCleaner.sh
+    # Fail if it still doesn't exist
+    if [ -z "$profile_dir" ]; then
+        echo "Failed to create .arkenfox profile."
+        exit 1
+    fi
+fi
 
-unset basedir
+# Symlink user-overrides.js to profile directory
+target="$profile_dir/user-overrides.js"
+if [ ! -e "$target" ]; then
+    echo "Creating symlink: $target"
+    ln -s "$user_overrides_source" "$target"
+else
+    echo "Symlink or file already exists: $target"
+fi
+
+pushd "$profile_dir" || exit
+
+echo "Downloading updater.sh to $(pwd)"
+curl -o updater.sh https://raw.githubusercontent.com/arkenfox/user.js/master/updater.sh
+
+echo "Downloading prefsCleaner.sh to $(pwd)"
+curl -o prefsCleaner.sh https://raw.githubusercontent.com/arkenfox/user.js/master/prefsCleaner.sh
+
+bash updater.sh -s -u
+bash prefsCleaner.sh -s
+
+popd
