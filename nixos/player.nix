74a67d7bbbd349fe3d60d338b66db60839f49426
sturdy3299 <c6k6z0zf@anonaddy.me>
2025-07-08 23:37:49 +0200

    update syncthing config

--- nixos/player.nix
+++ nixos/player.nix
@@ -10,6 +10,7 @@
 
   services.syncthing = {
     enable = true;
+    openDefaultPorts = true;
     user = "user";
     dataDir = "/home/user";
     settings.devices.steamdeck.id = "2HHZQDW-2LYDBPN-AYIKXOV-BVYJURA-CZUFCXF-7TB4N5Q-W2FE36H-YRXUMAM";
