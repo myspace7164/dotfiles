aeb67ba123e4f6c1a9121e3a60e1a391751ef0fb
sturdy3299 <c6k6z0zf@anonaddy.me>
2025-08-16 12:38:17 +0300

    Move remaining config from system/

--- nixos/home/user/home.nix
+++ nixos/home/user/home.nix
@@ -17,14 +17,23 @@
 
     # .config
     ".config/beets/config.yaml".source = ../../modules/beets/config.yaml;
+    ".config/git/config".source = ../../modules/git/config;
     ".config/i3status/config".source = ../../modules/i3status/config;
     ".config/kanshi/config".source = ../../modules/kanshi/config;
     ".config/mako/config".source = ../../modules/mako/config;
     ".config/nvim/init.lua".source = ../../modules/nvim/init.lua;
     ".config/sway/config".source = ../../modules/sway/config;
-    ".config/systemd/user/unison-drive.service".source = ../../modules/systemd/unison-drive.service;
-    ".config/systemd/user/unison-usb.service".source = ../../modules/systemd/unison-usb.service;
     ".config/tmux/tmux.conf".source = ../../modules/tmux/tmux.conf;
     ".config/wezterm/wezterm.lua".source = ../../modules/wezterm/wezterm.lua;
+
+    # mail
+    ".mbsyncrc".source = ../../modules/mail/.mbsyncrc;
+    ".msmtprc".source = ../../modules/mail/.msmtprc;
+
+    # unison
+    ".config/systemd/user/unison-drive.service".source = ../../modules/unison/unison-drive.service;
+    ".config/systemd/user/unison-usb.service".source = ../../modules/unison/unison-usb.service;
+    ".unison/backup-drive.prf".source = ../../modules/unison/backup-drive.prf;
+    ".unison/backup-usb.prf".source = ../../modules/unison/backup-usb.prf;
   };
 }
