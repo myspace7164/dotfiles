4a525d53f8cb1913052923e101fc8cbcd06b57b6
sturdy3299 <c6k6z0zf@anonaddy.me>
2025-08-16 11:48:38 +0300

    Move emacs config

--- nixos/modules/emacs/home.nix
+++ nixos/modules/emacs/home.nix
@@ -1,14 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  xdg.desktopEntries.org-protocol = {
-    name = "org-protocol";
-    comment = "Intercept calls from emacsclient to trigger custom actions";
-    categories = [ "Utility" ];
-    icon = "emacs";
-    type = "Application";
-    exec = "emacsclient -- %u";
-    terminal = false;
-    mimeType = [ "x-scheme-handler/org-protocol" ];
-  };
+  home.file.".emacs.d/init.el".source = ./init.el;
+  home.file.".emacs.d/early-init.el".source = ./early-init.el;
+  home.file.".local/share/applications/org-protocol.desktop".source = ./org-protocol.desktop;
 }
