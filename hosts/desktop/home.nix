54f147e9f5e5b330508d2a4d7930eea7d612b430
sturdy3299 <c6k6z0zf@anonaddy.me>
2025-09-09 15:33:30 +0200

    Rename some stuff, update lock

---
+++ hosts/desktop/home.nix
@@ -0,0 +1,5 @@
+{
+  xdg.configFile = {
+    "alacritty/current_theme.toml".source = ../../themes/alacritty/modus_vivendi.toml;
+  };
+}
--- scripts/toggle-theme.sh
+++ scripts/toggle-theme.sh
@@ -7,13 +7,13 @@ if [[ $current_state == "dark" ]]; then
     emacsclient -e "(load-theme 'modus-operandi)"
     gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
-    ln -sf ~/.local/share/themes/alacritty/modus-operandi.toml ~/.config/alacritty/current_theme.toml
+    ln -sf ~/.local/share/themes/alacritty/modus_operandi.toml ~/.config/alacritty/current_theme.toml
 else
     echo "dark" > ~/.local/state/theme
     emacsclient -e "(load-theme 'modus-vivendi)"
     gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-    ln -sf ~/.local/share/themes/alacritty/modus-vivendi.toml ~/.config/alacritty/current_theme.toml
+    ln -sf ~/.local/share/themes/alacritty/modus_vivendi.toml ~/.config/alacritty/current_theme.toml
 fi
 
 touch -h ~/.config/alacritty/alacritty.toml
