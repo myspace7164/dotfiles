c237d275942e3e072cd209d54806b7da5a6a071f
sturdy3299 <c6k6z0zf@anonaddy.me>
2025-09-10 15:42:05 +0200

    Update player config

--- hosts/player/default.nix
+++ hosts/player/default.nix
@@ -18,7 +18,9 @@
     user = "user";
     dataDir = "/home/user";
     settings.devices.steamdeck.id = "2HHZQDW-2LYDBPN-AYIKXOV-BVYJURA-CZUFCXF-7TB4N5Q-W2FE36H-YRXUMAM";
+    settings.devices.phone.id = "Y7ATORA-7CQMLAN-XLYMSCB-RKCUWRJ-CCBOW4E-A3WM3GZ-FIH3PLW-JT6OTQL";
     settings.folders."~/cloud/games".devices = [ "steamdeck" ];
+    settings.folders."~/cloud/org".devices = [ "phone" ];
   };
 
   system.autoUpgrade.allowReboot = true;
