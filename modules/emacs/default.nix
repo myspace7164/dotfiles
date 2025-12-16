ab1894749520356dc6ba1b46c173fecb03e79815
sturdy3299 <c6k6z0zf@anonaddy.me>
2025-12-16 20:32:35 +0100

    ran a bigger circle this time

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -1,35 +1,79 @@
 { pkgs, ... }:
 {
+  services.emacs = {
+    enable = true;
+    install = true;
+    package =
+      with pkgs;
+      ((emacsPackagesFor emacs-pgtk).emacsWithPackages (
+        epkgs:
+        (with epkgs.elpaPackages; [
+          auctex
+          gcmh
+          cape
+          consult
+          corfu
+          csv-mode
+          denote
+          djvu
+          embark
+          embark-consult
+          json-mode
+          marginalia
+          matlab-mode
+          orderless
+          vertico
+        ])
+        ++ (with epkgs.melpaPackages; [
+          cdlatex
+          citar
+          citar-denote
+          citar-embark
+          direnv
+          lua-mode
+          magit
+          markdown-mode
+          minions
+          nix-mode
+          org-caldav
+          org-contacts
+          plantuml-mode
+          yaml-mode
+        ])
+        ++ [
+          epkgs.mu4e
+          epkgs.treesit-grammars.with-all-grammars
+          epkgs.ripgrep
+        ]
+      ));
+    startWithGraphical = true;
+  };
+
+  environment.variables = {
+    ALTERNATE_EDITOR = "";
+    EDITOR = "emacsclient -t";
+    VISUAL = "emacsclient -c -a emacs";
+  };
+
   environment.systemPackages = with pkgs; [
-    ((emacsPackagesFor emacs-pgtk).emacsWithPackages (
-      epkgs:
-      (with epkgs.elpaPackages; [
-        cape
-        consult
-        corfu
-        csv-mode
-        embark
-        embark-consult
-        json-mode
-        marginalia
-        matlab-mode
-        orderless
-        vertico
-      ])
-      ++ (with epkgs.melpaPackages; [
-        direnv
-        lua-mode
-        magit
-        markdown-mode
-        minions
-        nix-mode
-        plantuml-mode
-        yaml-mode
-      ])
-      ++ [
-        epkgs.ripgrep
-        epkgs.treesit-grammars.with-all-grammars
+    (aspellWithDicts (
+      dicts: with dicts; [
+        de
+        en
+        en-computers
+        en-science
+        fi
+        sv
       ]
     ))
+    isync
+    msmtp
+    mu
   ];
+
+  services.protonmail-bridge = {
+    enable = true;
+    package = pkgs.protonmail-bridge;
+    path = with pkgs; [ gnome-keyring ];
+  };
 }
