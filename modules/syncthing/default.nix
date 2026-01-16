7745904e001ffb2db5eb26bcfd3c86f2b3a5b54e
sturdy3299 <c6k6z0zf@anonaddy.me>
2026-01-16 12:05:21 +0100

    update for pocket conf

--- modules/syncthing/default.nix
+++ modules/syncthing/default.nix
@@ -10,6 +10,7 @@
       desktop.id = "QGPMTZJ-RKIH4T6-AWTM5LT-GAWTTAP-PTYR3Z7-UNQKIPC-ENS5JN4-W635BAV";
       device.id = "V2RPWUX-YHTMNN7-OV324QC-5S56VI7-NLZQIWI-JVMGL4P-VEJFVCQ-66IF5A4";
       player.id = "MQPAXMC-J25TPXE-2TPZUWJ-RHRRSEM-SUZBLQQ-QHGL7YY-O52KPPT-CDXTBQO";
+      pocket.id = "GXR6U4A-4FDR47D-5EUJUMI-YCQMIOD-K2YAIOD-X2PUZ4Y-RXC57PU-L3ZTZQY";
       steamdeck.id = "2HHZQDW-2LYDBPN-AYIKXOV-BVYJURA-CZUFCXF-7TB4N5Q-W2FE36H-YRXUMAM";
       thinkpad.id = "6EF3BXL-PNMEQHU-6Z6GUAS-6QWSCEL-J37NOFE-YOJVMR4-QU76ERV-N7JEJAO";
     };
@@ -21,6 +22,7 @@
           "desktop"
           "device"
           "player"
+          "pocket"
           "steamdeck"
           "thinkpad"
         ];
