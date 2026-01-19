402fef00137d5c900bfdedba75341e96c330df1f
sturdy3299 <c6k6z0zf@anonaddy.me>
2026-01-19 08:44:56 +0100

    udpate pocket config

--- hosts/pocket/default.nix
+++ hosts/pocket/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -12,8 +12,20 @@
 
   time.timeZone = "Europe/Zurich";
 
-  services.tlp.enable = true;
-
-  console.keyMap = "us";
+  console = {
+    keyMap = "us";
+    font = "ter-128b";
+    packages = [ pkgs.terminus_font ];
+  };
   services.xserver.xkb.layout = "us";
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      DISK_DEVICES = "mmcblk0";
+      DISK_IOSCHED = "deadline";
+      WIFI_PWR_ON_AC = "off";
+      WIFI_PWR_ON_BAT = "off";
+    };
+  };
 }
