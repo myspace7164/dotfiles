5ec9330e86f23eec67f454df63c91e803d1a5727
sturdy3299 <c6k6z0zf@anonaddy.me>
2026-02-03 13:32:08 +0100

    update

--- install.sh
+++ install.sh
@@ -11,3 +11,5 @@ command -v swaymsg >/dev/null 2>&1 && swaymsg reload
 # If default ssh key does not exist, generate one and print it
 [[ ! -f $HOME/.ssh/id_rsa ]] && ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/id_rsa -N ""
 cat $HOME/.ssh/id_rsa.pub
+
+command -v syncthing >/dev/null 2>&1 && syncthing device-id
