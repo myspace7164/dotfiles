7254f7ea3586240f07a84633b2b302f4a80506b1
sturdy3299 <c6k6z0zf@anonaddy.me>
2026-02-11 20:08:36 +0100

    update

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -87,12 +87,9 @@
     "application/pdf" = "org.pwmt.zathura.desktop";
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
   programs.gnupg.agent = {
     enable = true;
-    # enableSSHSupport = true;
+    enableSSHSupport = true;
   };
 
   programs.localsend.enable = true;
